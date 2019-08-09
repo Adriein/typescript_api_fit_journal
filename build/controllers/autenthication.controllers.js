73cbd50263bf5d16a98ab594ffaa407098d6f4b2
Adriein <adria.claret@gmail.com>
2019-08-09 17:04:42 +0200

    implementation of login with jwt

--- build/controllers/autenthication.controllers.js
+++ build/controllers/autenthication.controllers.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("../database/database");
+const jwt = require('jsonwebtoken');
 function login(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const { email, password } = req.body;
@@ -21,17 +22,26 @@ function login(req, res) {
         try {
             const conn = yield database_1.connect();
             const existingEmail = yield conn.query('SELECT email FROM user_profile WHERE email = ?', email);
-            console.log(existingEmail[0]);
-            if (!existingEmail[0]) {
+            const stringyfiedEmail = yield JSON.stringify(existingEmail[0]);
+            const finalEmail = yield JSON.parse(stringyfiedEmail);
+            if (finalEmail.length < 1) {
+                res.status(500).json({ msg: 'This email not exists in our database' });
+            }
+            const user = yield conn.query('SELECT id, email, password, first_name, rol, sex, user_status FROM user_profile WHERE password = ?', password);
+            const stringyfiedUser = yield JSON.stringify(user[0]);
+            const finalUser = yield JSON.parse(stringyfiedUser);
+            if (finalUser.length < 1) {
                 res.status(500).json({ msg: 'User with this credentials not found' });
             }
-            const user = yield conn.query('SELECT id,email,first_name,rol,sex,user_status FROM user_profile WHERE password = ?', password);
-            console.log(user[0]);
-            /*jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+            jwt.sign({ user }, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ msg: 'Internal server error' });
+                }
                 res.json({
-                  token
-                });*/
-            return res.json(user[0]);
+                    token
+                });
+            });
         }
         catch (err) {
             console.log(err);
--- src/controllers/autenthication.controllers.ts
+++ src/controllers/autenthication.controllers.ts
@@ -1,39 +1,53 @@
 import { Request, Response } from 'express';
-
 import { connect } from '../database/database'
 
-export async function login(req: Request, res: Response): Promise <Response>{
+const jwt = require('jsonwebtoken');
 
-    const { email, password} = req.body;
 
-    if(!email){
-        res.status(500).json({ msg : 'You must introduce your email'});
+export async function login(req: Request, res: Response): Promise<Response | void> {
+
+    const { email, password } = req.body;
+
+    if (!email) {
+        res.status(500).json({ msg: 'You must introduce your email' });
     }
-    if(!password){
-        res.status(500).json({ msg : 'You must introduce your password'});
+    if (!password) {
+        res.status(500).json({ msg: 'You must introduce your password' });
     }
 
-    try{
+    try {
         const conn = await connect();
         const existingEmail = await conn.query('SELECT email FROM user_profile WHERE email = ?', email);
-        console.log(existingEmail[0]);
-        
-        if(!existingEmail[0]){
-            res.status(500).json({ msg : 'User with this credentials not found'});
+
+        const stringyfiedEmail = await JSON.stringify(existingEmail[0]);
+        const finalEmail = await JSON.parse(stringyfiedEmail);
+
+        if (finalEmail.length < 1) {
+            res.status(500).json({ msg: 'This email not exists in our database' });
         }
 
-        const user = await conn.query('SELECT id,email,first_name,rol,sex,user_status FROM user_profile WHERE password = ?', password);
-        console.log(user[0]);
-        
-        /*jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+        const user = await conn.query('SELECT id, email, password, first_name, rol, sex, user_status FROM user_profile WHERE password = ?', password);
+
+        const stringyfiedUser = await JSON.stringify(user[0]);
+        const finalUser = await JSON.parse(stringyfiedUser);
+
+        if (finalUser.length < 1) {
+            res.status(500).json({ msg: 'User with this credentials not found' });
+        }
+
+        jwt.sign({ user }, 'secretkey', { expiresIn: '30s' }, (err: any, token: any) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ msg: 'Internal server error' });
+            }
             res.json({
-              token
-            });*/
-        return res.json(user[0]);
-    
-    }catch(err){
+                token
+            });
+        });
+
+    } catch (err) {
         console.log(err);
-        return res.status(500).json({ msg: 'Internal server error'});
-        
-    } 
+        return res.status(500).json({ msg: 'Internal server error' });
+
+    }
 }
